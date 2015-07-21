856ee2c0ca461cefc608d7b73a24da58cbc902c4
Taylor Lea <tlea105@gmail.com>
2015-07-20 19:38:16 -0700

    Added admin login
    Admin login added with salt and MD5. The username, salt and password is
    hard-coded into the database. CRITICAL: I added a column to the admin
    table in our DB. The column is named ‘salt’.

--- application/controllers/Mains.php
+++ application/controllers/Mains.php
@@ -4,6 +4,30 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Mains extends CI_Controller {
 	public function index()
 	{		
-		$this->load->view('show');
+		$this->load->view('order_info');
 	}
+    public function admin(){/*how do i go just directly to admin without having mains in the url?*/
+        $this->load->view('admin_login');
+    }
+    public function admindash(){
+        if ($this->session->userdata('admin') == 'in') {
+            $this->load->view('dashboard_orders');/*This if statement is to ensure that the admin is logged in so nobody can just type the url to get adin power*/
+        }else{
+            redirect('/');
+        }
+    }
+
+    public function login(){ /*let's send the password and email over to our model!*/
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+        $login_check = $this->Model->login($email, $password);
+        if ($login_check) {
+            $this->session->set_userdata('admin', 'in'); /*setting the admin status to logged in*/
+            redirect('admindash');
+        }else{
+            $this->session->set_userdata('admin','out'); /*setting the admin status to logged out NOTE probably unnecessary*/
+            $this->session->set_flashdata('errors','Email/Password combination incorrect');
+            redirect('/mains/admin'); /*how do i go just directly to admin without having mains in the url?*/
+        }
+    }
 }
