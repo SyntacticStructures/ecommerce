bc932ce39523aaa4f065ea94aa45daa2d4f980d3
rgoodm2 <rachelgoodman94@gmail.com>
2015-07-21 10:04:08 -0700

    committed minor changes
    just a few changes with the addition of stripe and form validation

--- application/models/Main.php
+++ application/models/Main.php
@@ -1,21 +1,21 @@
 <?php 
 
-class Course extends CI_Model {
-     function get_all_courses()
+class Main extends CI_Model {
+     function get_all_products()
      {
          return $this->db->query("SELECT * FROM courses")->result_array();
      }
-     function get_course_by_id($course_id)
+     function get_product_by_id($course_id)
      {
          return $this->db->query("SELECT * FROM courses WHERE id = ?", array($course_id))->row_array();
      }
-     function add_course($course)
+     function add_product($course)
      {
          $query = "INSERT INTO Courses (name, description, created_at) VALUES (?,?,?)";
          $values = array($course['name'], $course['description'], date("Y-m-d, H:i:s")); 
          return $this->db->query($query, $values);
      }
-     function remove_course($id)
+     function remove_product($id)
      {
          $query = "DELETE FROM Courses WHERE id = ?";
          return $this->db->query($query, $id);
