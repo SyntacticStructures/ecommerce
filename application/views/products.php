eb287f123b635c029466c8b560da89811cb0d6a7
Luke_Caddy <lukecaddy@gmail.com>
2015-07-21 15:18:46 -0700

    products style
    added style to products.php

--- application/views/products.php
+++ application/views/products.php
@@ -4,41 +4,33 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
-
-	<style type="text/css">
-
-	</style>
+	<title>La Mode</title>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script> 
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="<?php echo base_url("assets/css/css/bootstrap.css"); ?>" />
+	<link rel="stylesheet" href="<?php echo base_url("assets/css/css/products_css/style.css"); ?>" />
+	<meta name=viewport content='width=700'>
 </head>
-<body>
-	<div><!-- This is topnav -->
-		<h1>Dojo eCommerce</h1>
-		<a>Shopping Cart <?php //add number in php?></a>
-	</div>
-<div id="container">
-		<div>
-			<form><!-- needs action and method -->
-				<input type="search" name="search" placeholder="product name"> <!-- we may need to change this to a text input and add a submit input -->
-			</form>
-			<p>Categorgies</p>
-			<a href="">Shirts</a>
-			<a href="">Shoes</a>
-			<a href="">Shorts/Skirts</a>
-			<a href="">Dresses</a>
-			<a href="">Jewlery</a>
-			<a href="">Show all!!</a>
-		</div>
-		<div>
-			<h4>Title</h4>
-			<!-- add in pictures and links to product pages -->
-			<ul><!-- here are the links to all the pages of products -->
-				<li>
-					<a href="/">1</a><!-- example links -->
-					<a href="/">2</a>
-				</li>
+<body  class = "container">
+	<div class = "container">
+		<div class = "header">
+			<h1 class = "header_text">LA MODE</h1>
+		<div class = "sidebar">
+			<ul class = "list">
+				<li class = "listitem"><a class = "sidebar_text" href="">ALL PRODUCTS</a></li>
+				<li class = "listitem"><a class = "sidebar_text" href="">SHIRTS</a></li>
+				<li class = "listitem"><a class = "sidebar_text" href="">SHOES</a></li>
+				<li class = "listitem"><a class = "sidebar_text" href="">SHORTS / SKIRTS</a></li>
+				<li class = "listitem"><a class = "sidebar_text" href="">DRESSES</a></li>
+				<li class = "lastitem"><a class = "sidebar_text" href="">JEWELRY</a></li>
 			</ul>
+				<input type = "text" placeholder = "SEARCH" class = "search">
+				<p class = "shoppingcart_text"><a class = "shoppingcart_text" href="">SHOPPING CART (5)</a></p>
 		</div>
-</div>
-
+		<div class = "header_image">
+			<img src="http://www.martaphotographer.com/wp-content/uploads/2012/05/052.jpg" height="719" width="1078" align="middle">
+		</div>
+		<!-- FEATURED PRODUCTS -->
+	</div>
 </body>
 </html>
