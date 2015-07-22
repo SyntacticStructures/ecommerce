874203a980763d58f36126079e989f7b2ee7d6dd
Luke_Caddy <lukecaddy@gmail.com>
2015-07-21 17:33:52 -0700

    Merge branch 'Luke'
    Conflicts:
    	application/controllers/Mains.php
    	application/views/admin_login.php

--- application/views/admin_login.php
+++ application/views/admin_login.php
@@ -2,8 +2,13 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title></title>
+	<title>Adminstrative Login</title>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script> 
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="<?php echo base_url("assets/css/css/bootstrap.css"); ?>" />
+	<link rel="stylesheet" href="<?php echo base_url("assets/css/css/style.css"); ?>" />
 </head>
+<<<<<<< HEAD
 <body>
 <h3>Admin Login Page</h3>
 <?= $this->session->flashdata('errors'); ?>
@@ -12,5 +17,22 @@
 	<label>Password: <input type="password" name="password"></label>
 	<input type="submit" name="submit" value="login">
 </form>
+=======
+<body class="background">
+<div class="container">
+    <div class="row">
+        <div id="login" class="col-sm-6 col-md-4 col-md-offset-4">
+            <h1 id = "header" class="text-center login-title">Admin Login</h1>
+            <div class="account-wall">
+                <form class="form-signin">
+	                <input type="text" class="form-control" placeholder="Email" required autofocus>
+	                <input type="password" class="form-control" placeholder="Password" required>
+	                <button class="btn btn-lg btn-primary btn-block" type="submit"><span class="text">Login</span></button>
+              	</form>
+    		</div>
+		</div>
+	</div>
+</div>
+>>>>>>> Luke
 </body>
 </html>
